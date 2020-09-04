370a109ae5c58f1384b689aab02897f86fee1feb
Martin Magsombol <martinremags@gmail.com>
2020-09-04 16:09:26 -0700

    Testing with apollo-server-express

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-/**
- * TODO:
- * - Create PhotoType, PhotoModel, PhotoResolver
- *      - PhotoResolver: connects to s3 bucket and uploads a file from user's
- *        own files. Takes filepath and adds to photoobject to save to db.
- * - Update documentation on how models, schema, and resolvers work together
- * - Naive implementation of authentication
- *     - Hashing Algorithm and hashing passwords
- * - Create unit tests for each resolver (uploading files)
- * - Replace current postgresql server with heroku/AWS postgresql server
- * - Create AWS S3 Bucket for storing photos
- **/
-
-const { ApolloServer } = require("apollo-server");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 
 // Create instance of ApolloServer
 const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+server.applyMiddleware({ app });
 
 // Run Server
 const PORT = process.env.PORT || 4000;
-server.listen({ port: PORT });
+app.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
+});
