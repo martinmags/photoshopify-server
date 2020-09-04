3391caefd41e4db8fbd47e04b24045199a55eef5
Martin Magsombol <martinremags@gmail.com>
2020-09-04 16:00:05 -0700

    Check if heroku fixed

--- config/db.js
+++ config/db.js
@@ -1,22 +1,16 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-
-  {
-    dialect: "postgres",
-    host: process.env.DB_HOST,
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const sequelize = new Sequelize(process.env.DB_URI, {
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
 const test = async function () {
   try {
