d7409787510735f60c60517b6df131e85ebb65c7
Martin Magsombol <martinremags@gmail.com>
2020-09-09 05:24:56 -0700

    uh

--- resolvers/PhotoResolver.js
+++ resolvers/PhotoResolver.js
@@ -51,11 +51,9 @@ module.exports = {
       }
 
       // Upload to Cloudinary
-
       const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
         upload_preset: "photoshopify",
       });
-      console.log("UPLOADED:", uploadedResponse);
 
       // Extract the public_id from cloudinary
       const filepublicid = uploadedResponse.public_id;
